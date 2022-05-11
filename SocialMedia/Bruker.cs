95143b0ed76b5b83c23cc9a6e204c71eb105b92a
Ben David Le <ben@getacademy.no>
2022-05-11 11:21:11 +0200

    Add project files.

---
+++ SocialMedia/Bruker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FriendFace
+{
+    internal class Bruker
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public List<Bruker> Friends { get; } = new List<Bruker>();
+
+        //Constructor
+        public Bruker(string name, string email, int age, string sex)
+        {
+            Name = name;
+            Email = email;
+            Age = age;
+            Sex = sex;
+        }
+
+        //Metoder
+        public void ShowProfile()
+        {
+            Console.WriteLine($"Hei, { Name }!");
+            Console.WriteLine($"Email: { Email }");
+            Console.WriteLine($"Age: { Age }");
+            Console.WriteLine($"Sex: { Sex }");
+        }
+
+        public void ShowFriends()
+        {
+            Console.WriteLine("Dine venner: ");
+            foreach (Bruker friend in Friends)
+            {
+                Console.WriteLine(friend.Name);
+            }
+        }
+
+        public void ViewFriend(Bruker friend)
+        {
+            Console.WriteLine($"Din venn { friend.Name }!");
+            Console.WriteLine($"Email: { friend.Email }");
+            Console.WriteLine($"Age: { friend.Age }");
+            Console.WriteLine($"Sex: { friend.Sex }");
+        }
+
+        public void AddFriend(Bruker friend)
+        {
+            Friends.Add(friend);
+            Console.WriteLine($"La til { friend.Name } som venn.");
+        }
+
+        public void RemoveFriend(Bruker friend)
+        {
+
+            for (int i = 0; i < Friends.Count; i++)
+            {
+                if (friend.Name == Friends[i].Name)
+                {
+                    Friends.RemoveAt(i);
+                    Console.WriteLine($"Fjernet { friend.Name } som venn.");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ SocialMedia/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace FriendFace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bruker Ben = new Bruker("Ben David", "asd@asd.com", 22, "Male");
+            Bruker Frank = new Bruker("Frank", "frank@asd.com", 27, "Male");
+            Bruker Thomas = new Bruker("Thomas", "Thomas@asd.com", 30, "Male");
+            Bruker Terje = new Bruker("Terje", "Terje@asd.com", 47, "Male");
+            Bruker Therese = new Bruker("Therese", "asd@asd.com", 28, "Female");
+
+            Bruker[] AllUsers = new Bruker[] { Ben, Frank, Thomas, Terje, Therese };
+
+            Bruker LoggedinnUser = Frank;
+
+            LoggedinnUser.ShowProfile();
+
+
+            while (true)
+            {
+                string kommando = Console.ReadLine().ToLower();
+                string[] splitKommando = kommando.Split(" ");
+                Console.Clear();
+                LoggedinnUser.ShowProfile();
+
+
+                switch (splitKommando[0])
+                {
+                    case "add":
+                        LoggedinnUser.AddFriend(GetUserFromName(splitKommando[1], AllUsers));
+                        break;
+                    case "remove":
+                        LoggedinnUser.RemoveFriend(GetUserFromName(splitKommando[1], AllUsers));
+                        break;
+                    case "friends":
+                        LoggedinnUser.ShowFriends();
+                        break;
+                    case "view":
+                        LoggedinnUser.ViewFriend(GetUserFromName(splitKommando[1], AllUsers));
+                        break;
+                    default:
+                        Console.WriteLine("Kommando er ikke tilgjenglig.");
+                        break;
+                }
+            }
+
+
+            static Bruker GetUserFromName(string name, Bruker[] users)
+            {
+                foreach (Bruker bruker in users)
+                {
+                    if (bruker.Name.ToLower().Contains(name))
+                    {
+                        return bruker;
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
